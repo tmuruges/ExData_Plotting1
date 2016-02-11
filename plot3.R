83fdfbad8d9d23c83bbd6cae3029fd528b07d85d
tmuruges <mthangavel28@gmail.com>
2016-02-11 17:38:25 +0530

    Create plot4.png

--- plot3.R
+++ plot3.R
@@ -1,1 +1,16 @@
+dataFile <- "./household_power_consumption.txt"
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 
+datetime <- strptime(paste(subData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+globalActivePower <- as.numeric(subData$Global_active_power)
+metering1 <- as.numeric(subData$Sub_metering_1)
+metering2 <- as.numeric(subData$Sub_metering_2)
+metering3 <- as.numeric(subData$Sub_metering_3)
+
+png("plot3.png", width=480, height=480)
+plot(datetime, metering1, type="l", ylab="Energy Submetering", xlab="")
+lines(datetime, metering2, type="l", col="red")
+lines(datetime, metering3, type="l", col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
+dev.off()
